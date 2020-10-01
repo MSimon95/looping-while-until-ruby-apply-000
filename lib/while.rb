a8e0808dd89671a13af0743c6aef71dbd6a03e3f
madiwa Simon <mad.simon01@gmail.com>
2020-10-01 14:15:12 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,8 @@
 def using_while
   levitation_force = 6
-	while levitation_force > 10
+	while levitation_force < 10
+		levitation_force += 1
 	puts "Wingardium Leviosa, current levitation force #{levitation_force} "
-	
-	levitation_force += 1
   #your code here
 end
 end
